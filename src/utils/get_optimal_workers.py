e4676bed85c8f79055fac895a38824bac98337c8
maximilian.frantzen@unternehmertum.de <maximilian.frantzen@unternehmertum.de>
2022-12-08 11:16:36 -0500

    update

--- src/utils/get_optimal_workers.py
+++ src/utils/get_optimal_workers.py
@@ -11,15 +11,16 @@ root = pyrootutils.setup_root(
 from time import time
 import multiprocessing as mp
 from src.datamodules.focus_datamodule import FocusDataModule
+from src.datamodules.components.focus_dataset import FocusDataset
 from torch.utils.data import DataLoader
 
-path = "/n/data2/hms/dbmi/kyu/lab/maf4031/focus_dataset/Inflammation2_665716"
-batch_size = 32
+path = "/Volumes/FOCUS/Inflammation2_665716"
+batch_size = 1
 
 optimal_time = None
 optimal_num_workers = None
 for num_workers in range(2, mp.cpu_count(), 2):  
-    train_loader = DataLoader(FocusDataModule(path), shuffle=True, num_workers=num_workers, batch_size=batch_size, pin_memory=True)
+    train_loader = DataLoader(FocusDataset(path, subsample=True, subsample_size=1), shuffle=True, num_workers=num_workers, batch_size=batch_size, pin_memory=True)
     start = time()
     for epoch in range(1, 3):
         for i, data in enumerate(train_loader, 0):
