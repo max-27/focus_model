85314b853fb92f5101880299ff9ba786d1e7948e
max-27 <maxfrantzen@gmx.de>
2022-12-11 18:58:44 -0500

    feat: new job summit files

---
+++ single_run_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -c 8
+#SBATCH -t 24:00:00
+#SBATCH -p gpu_quad
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G
+#SBATCH -o logs/hostname_%j.out
+#SBATCH -e logs/hostname_%j.err
+
+module load gcc/6.2.0
+module load cuda/10.1
+module load miniconda3/4.10.3
+source activate focus
+python src/train.py
