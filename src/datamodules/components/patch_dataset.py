cd181f4543f15c5c951a545a351b82f6c62cebd5
max-27 <maxfrantzen@gmx.de>
2022-12-15 16:14:17 -0500

    feat: load data in multiple processes

--- src/datamodules/components/patch_dataset.py
+++ src/datamodules/components/patch_dataset.py
@@ -19,24 +19,22 @@ from torchvision.transforms import transforms
 import re
 from typing import List, Tuple, Dict
 from src.utils.color_filter import ColorFilter
+from src.datamodules.components.focus_dataset import FocusDataset
+import itertools
+from concurrent.futures import ProcessPoolExecutor
+from collections import ChainMap
 
 
-class FocusDataset(Dataset):
+class PatchFocusDataset(Dataset):
     def __init__(
         self,
         data_dir: str = "data/",
         transform=None, 
-        subsample: bool = False, 
         subsample_size: int = 20, 
         image_size: List = [720, 1280],
         patch_size: List =  [360, 256], 
-        patch_num: int = 10,
         ) -> None:
         super().__init__()
-        self.color_filter = ColorFilter()
-        self.data_dir = data_dir
-        self.transform = transform
-
         h, w = image_size
         x_steps = int(h / patch_size[0])
         y_steps = int(w / patch_size[1])
@@ -46,43 +44,110 @@ class FocusDataset(Dataset):
         self.patch_size = patch_size
         self.label_dict = None
 
-    def __len__(self):
-        return len(self.array_labels)
+        self.color_filter = ColorFilter()
+        self.transform = transform
+        self.data_dir = data_dir
+        self.transform = transform
+        self.array_images, self.array_labels = self._find_files_by_sample(subsample_size=subsample_size)
+        self.patch_images, self.patch_labels = self._get_patch_directories()
 
-    def __getitem__(self, index) -> Tuple[torch.Tensor, torch.Tensor]:
-        return super().__getitem__(index)
+    def __len__(self):
+        return len(self.patch_labels)
+
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
+        img_path, x, y = self.patch_images[idx]
+        img = np.array(io.imread(img_path))
+        patch = img[int(x-int(self.patch_size[0]/2)):int(x+int(self.patch_size[0]/2)), int(y-int(self.patch_size[1]/2)):int(y+int(self.patch_size[1]/2))]
+        label = np.array([self.patch_labels[idx] * 3.4])  # mutliple by 3.4 to get distance in um
+        return self.transform(patch), torch.from_numpy(label)
+
+    def _find_files_by_sample(self, subsample_size: int = 20) -> List:
+        array_images = []
+        array_labels = []
+        for sample_box in next(os.walk(self.data_dir))[1]:
+            samples = glob.glob(os.path.join(self.data_dir, sample_box,'sample*'))
+            for sample in samples:
+                images = glob.glob(os.path.join(sample,'*distance*.jpg'))
+                labels = [int(re.findall(r"-?\d+", path)[-1]) for path in images]
+                sampled_images, samples_labels = self.sample_images(images, labels, subsample_size)
+                array_images.extend(sampled_images)
+                array_labels.extend(samples_labels)
+        return array_images, array_labels
+    
+    def sample_images(self, images: List, labels: List, subsample_size: int = 50) -> Tuple[List, List]:
+        """Sample images with positive and negative distance values."""
+        images_pos_distance = [path for path in images if int(re.findall(r"-?\d+", path)[-1]) > 0]
+        images_neg_distance = [path for path in images if int(re.findall(r"-?\d+", path)[-1]) < 0]
+        optimal_img = [path for path in images if int(re.findall(r"-?\d+", path)[-1]) == 0][0]
+
+        num_pos_indices = subsample_size if subsample_size < len(images_pos_distance) else len(images_pos_distance)
+        num_neg_indices = subsample_size if subsample_size < len(images_neg_distance) else len(images_neg_distance)
+        pos_indices = np.random.permutation(len(images_pos_distance))[:num_pos_indices]
+        neg_indices = np.random.permutation(len(images_neg_distance))[:num_neg_indices]
+        pos_images = [images_pos_distance[i] for i in pos_indices]
+        neg_images = [images_neg_distance[i] for i in neg_indices]
+
+        pos_labels = [int(re.findall(r"-?\d+", path)[-1]) for path in pos_images]
+        neg_labels = [int(re.findall(r"-?\d+", path)[-1]) for path in neg_images]
+        return [*pos_images, *neg_images, optimal_img], [*pos_labels, *neg_labels, 0]
+
 
     def brenner_gradient(self, img: np.array) -> int:
             return np.sum((img[2:]-img[:-2])**2)
 
-    def get_directories(self) -> Dict:
-        sample_box_dict = {sample_box: {} for sample_box in next(os.walk(self.data_dir))[1]}
-        for sample_box in next(os.walk(self.data_dir))[1]:
-            samples = glob.glob(os.path.join(self.data_dir, sample_box,'sample*'))
-            sample_dict = {sample: {} for sample in samples}
-            for sample in samples:
-                images = glob.glob(os.path.join(sample,'*distance*.jpg'))
-                image_dict = {patch: [0, 0, 0] for patch in range(len(self.patch_coords))}
-                for image in images:
-                    img = io.imread(image)
-                    for patch_idx, (x, y) in enumerate(self.patch_coords):
-                        patch = img[int(x-int(self.patch_size[0]/2)):int(x+int(self.patch_size[0]/2)), int(y-int(self.patch_size[1]/2)):int(y+int(self.patch_size[1]/2))]
-                        brenner_value = self.brenner_gradient(patch)
-                        if brenner_value > image_dict[patch_idx][-1]:
-                            image_dict[patch_idx] = x, y, brenner_value
-                sample_dict[sample] = image_dict
-            sample_box_dict[sample_box] = sample_dict
-            print(sample_box)
-        self.label_dict = sample_box_dict
-        return sample_box_dict
+    def _get_patch_label(self, sample: str) -> Dict:
+        """
+        Parameters
+        ----------
+        sample: str
+            Complete path to sample directory
+        
+        Returns
+        -------
+        image_dict: Dict
+            Dictionary of sample path, optimal image index and brenner gradient value for each patch.
+        """
+        images = glob.glob(os.path.join(sample,'*distance*.jpg'))
+        image_dict = {patch: [0, 0, 0] for patch in range(len(self.patch_coords))}
+        for image in images:
+            img = io.imread(image)
+            for patch_idx, (x, y) in enumerate(self.patch_coords):
+                patch = img[int(x-int(self.patch_size[0]/2)):int(x+int(self.patch_size[0]/2)), int(y-int(self.patch_size[1]/2)):int(y+int(self.patch_size[1]/2))]
+                brenner_value = self.brenner_gradient(patch)
+                if brenner_value > image_dict[patch_idx][-1]:
+                    img_idx = int(image.split('distance')[-1].split('.')[0])
+                    image_dict[patch_idx] = img_idx , brenner_value
+        return {sample: image_dict}  
+
+    def _get_patches(self) -> Dict:
+        sample_boxes = next(os.walk(self.data_dir))[1]
+        samples = list(itertools.chain.from_iterable([glob.glob(os.path.join(self.data_dir, sample_box,'sample*')) for sample_box in sample_boxes]))
+        with ProcessPoolExecutor(max_workers=61) as executor:
+                futures = executor.map(
+                    self._get_patch_label,
+                    samples,
+                )
+        return dict(ChainMap(*list(futures)))
+
+    def _get_patch_directories(self) -> Dict:
+        """Based on paths in array images get all patches for each sample."""
+        patch_images = []
+        patch_labels = []
+        patches_dict = self._get_patches()
+        for image in self.array_images:
+            patch_optimal_indices = patches_dict[image.split("/distance")[0]]
+            for patch_idx, (optimal_idx, _) in patch_optimal_indices.items():
+                if abs(optimal_idx) < 50:
+                    patch_images.append((image, self.patch_coords[patch_idx]))
+                    old_idx = int(image.split('distance')[-1].split('.')[0])
+                    patch_labels.append(old_idx - optimal_idx)
+        return patch_images, patch_labels
+
 
 if __name__ == "__main__":
     import time 
     pl.seed_everything(42, workers=True)
-    dataset = FocusDataset(data_dir="/n/data2/hms/dbmi/kyu/lab/maf4031/focus_dataset")
     start = time.time()
-    dict_out = dataset.get_directories()
-    total_time = time.time() - start
-    torch.save(dataset, f"/home/maf4031/focus_model/data/test_patch_dataset_{total_time}.pt")
-    a = 1
-
+    dataset = PatchFocusDataset(data_dir="/n/data2/hms/dbmi/kyu/lab/maf4031/focus_dataset", subsample_size=100)
+    print(time.time() - start)
+    torch.save(dataset, "/home/maf4031/focus_model/data/test1_patch_dataset.pt")
