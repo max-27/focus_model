be6ab4a6eb0f2db8a42e61f9bb266551ad8b52e1
max-27 <maxfrantzen@gmx.de>
2022-12-16 23:40:11 -0500

    feat: parallelize checking

--- src/utils/check_bad_data.py
+++ src/utils/check_bad_data.py
@@ -2,6 +2,7 @@ from skimage import io
 import glob 
 import os
 import numpy as np
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
 def check():
@@ -21,7 +22,30 @@ def check():
     with open("/home/maf4031/focus_model/src/utils/bad_img_list.npy", "wb") as f:
         np.save(f, bad_img_list)
 
+def _check_single_img(img):
+    try:
+        _ = io.imread(img)
+    except:
+        print(img)
+        return img
+    return None
+
+def check_optmized():
+    path = "/n/data2/hms/dbmi/kyu/lab/maf4031/focus_dataset/IDH/new_data"
+    array_images = glob.glob(os.path.join(path,'*/distance*.jpg'))
+    print(len(array_images))
+    with ProcessPoolExecutor(max_workers=61) as executor:
+        futures = executor.map(
+            _check_single_img,
+            array_images,
+        )
+    return list(filter(None, list(futures)))
 
 if __name__ == "__main__":
-    check()
+    #check()
+    bad_img_list = check_optmized()
+    if len(bad_img_list) == 0:
+        print("No corrupted files found")
+    with open("/home/maf4031/focus_model/src/utils/bad_img_list_IDH.npy", "wb") as f:
+        np.save(f, bad_img_list)
     #img = io.imread("/n/data2/hms/dbmi/kyu/lab/maf4031/focus_dataset/Inflammation_3_4/sample_17/distance9.jpg") 
